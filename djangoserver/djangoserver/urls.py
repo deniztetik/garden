88eea2d003211a93a67b730bd562e3af58356422
Deniz Tetik <deniztetik31@gmail.com>
2016-12-04 20:44:20 -0500

    feat(, ): Add webpack and create home Django app
    Modified webpack to optimize its use with django-webpack-loader. Some of the old contents that were
    
    removed are now in a comment and can be added back to the extent that they don't damage the
    
    environment. Also created a new "home" app to be the location that django serves up the frontend.

--- djangoserver/djangoserver/urls.py
+++ djangoserver/djangoserver/urls.py
@@ -1,4 +1,4 @@
-"""djangoserver URL Configuration
+"""garden URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,92 @@
-const webpack = require('webpack');
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  devtool: 'inline-source-map',
-  // context: __dirname + "/client",
-  entry: [
-    "webpack-hot-middleware",
-    "webpack/hot/only-dev-server",
-    path.join(__dirname, "client/main.js"),
-  ],
+  context: __dirname,
+
+  entry: './client/main', //entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    // publicPath: "/static/",
+    path: path.resolve('./assets/bundles'),
+    filename: "[name]-[hash].js",
   },
+
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new BundleTracker({filename: './webpack-stats.json'}),
   ],
-  watch: true,
+
   module: {
-    preloaders: [
-      {
-        test: [/\.js$/, /\.jsx$/],
-        exclude: /node_modules/,
-        loader: 'jshint-loader'
-      }
-    ],
     loaders: [
+      { test: /\.jsx?$/, exclude: /node_nodules/, loader: 'babel-loader'}, // to transform JSX into JS
       {
-        test: [/\.js$/, /\.jsx$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: ['react-hmre', 'react', 'es2015'],
-        }
+              test: /\.scss$/,
+              loaders: ['style', 'css', 'sass']
       },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      }
-    ]
+    ],
   },
+
   resolve: {
+    modulesDirectories: ['node_modules'],
     extensions: ['', '.js', '.jsx']
   },
-  devServer: {
-    hot: true,
-    historyApiFallback: true,
-  },
 };
-// "resolve" allows use of require('./logger') instead of require('./logger.js');
-// "test: [/\.js$/, /\.es6$/]"" allows for both .js and .es6 files to be passed to babel, allowing for the use of JSX in the code.
-// devtool makes it easier to debug - instead of giving us an error from bundle.js, we will see it in our source files.
+
+
+// const webpack = require('webpack');
+// const path = require('path');
+//
+// module.exports = {
+//   devtool: 'inline-source-map',
+//   // context: __dirname + "/client",
+//   entry: [
+//     "webpack-hot-middleware",
+//     "webpack/hot/only-dev-server",
+//     path.join(__dirname, "client/main.js"),
+//   ],
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "./dist"),
+//     // publicPath: "/static/",
+//   },
+//   plugins: [
+//     new webpack.optimize.OccurrenceOrderPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//     new webpack.NoErrorsPlugin(),
+//   ],
+//   watch: true,
+//   module: {
+//     preloaders: [
+//       {
+//         test: [/\.js$/, /\.jsx$/],
+//         exclude: /node_modules/,
+//         loader: 'jshint-loader'
+//       }
+//     ],
+//     loaders: [
+//       {
+//         test: [/\.js$/, /\.jsx$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           cacheDirectory: true,
+//           presets: ['react-hmre', 'react', 'es2015'],
+//         }
+//       },
+//       {
+//         test: /\.scss$/,
+//         loaders: ['style', 'css', 'sass']
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+//   devServer: {
+//     hot: true,
+//     historyApiFallback: true,
+//   },
+// };
+// // "resolve" allows use of require('./logger') instead of require('./logger.js');
+// // "test: [/\.js$/, /\.es6$/]"" allows for both .js and .es6 files to be passed to babel, allowing for the use of JSX in the code.
+// // devtool makes it easier to debug - instead of giving us an error from bundle.js, we will see it in our source files.
